baf0d14d47d693722519d9eb50347adb696a0220
vedhanayaki <vedhanayaki1996@gmail.com>
2017-07-08 12:39:33 +0530

    Add files via upload

---
+++ worldhello.java
@@ -0,0 +1,13 @@
+package Tree;
+
+public class worldhello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String s="hello";
+String a="world";
+String b=a+s;
+System.out.println(b+" ");
+	}
+
+}
